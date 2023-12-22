ae617095b832ab99350d6e997cd3eb71fc7154a4
FatimaIrshad <bibimemoona2017@gmail.com>
2023-12-22 10:33:18 +0500

    javascript practice

---
+++ Advance javascript/FilteringArrayOfObjects.js
@@ -0,0 +1,14 @@
+obj1 = [
+   {id:1,name:'fatima',description:'female'},
+{id:2,name:'javaria',description:'female'},
+{id:3,name:'muzammil',description:'male'},
+{id:4,name:'mudassir',description:'male'},
+
+];
+
+var result = obj1.filter(obj => obj.description == 'male');
+console.log(result);
+
+
+
+
