0b37f4d472c57becd5d4ef8e9d37f34471fd64fe
FatimaIrshad <bibimemoona2017@gmail.com>
2023-12-23 13:42:38 +0500

    Javascript Practice

---
+++ Advance javascript/calculator.js
@@ -0,0 +1,43 @@
+/*
+  Implement a class `Calculator` having below methods
+    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
+    - add: takes a number and adds it to the result
+    - subtract: takes a number and subtracts it from the result
+    - multiply: takes a number and multiply it to the result
+    - divide: takes a number and divide it to the result
+    - clear: makes the `result` variable to 0
+    - getResult: returns the value of `result` variable
+    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
+      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
+      Points to Note: 
+        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
+        2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
+
+  Once you've implemented the logic, test your code by running
+*/
+
+class Calculator {
+  constructor(str1 , str2 ){
+    this.str1 = str1;
+    this.str2 = str2;
+    
+  }
+  sum(){
+    return this.str1 + this.str2;
+  }
+  difference(){
+    return this.str1 - this.str2;
+  }
+  multiplication(){
+    return this.str1 * this.str2;
+  }
+  division(){
+    return this.str1 / this.str2;
+  }
+}
+let result = new Calculator(10,30)
+console.log(result.sum());
+console.log(result.difference());
+console.log(result.multiplication());
+console.log(result.division());
+module.exports = Calculator;
