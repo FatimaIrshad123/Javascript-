3d7dfdea1289ca9f25df22524c5136dd545b2442
FatimaIrshad <bibimemoona2017@gmail.com>
2024-01-16 13:10:49 +0500

    Checking Vowels

---
+++ basic-exercises/vowels-checking.js
@@ -0,0 +1,6 @@
+var input = "M";
+if (input == "A" || input == "E" || input == "I" || 
+input == "O" || input == "U") {
+    console.log("Its a vowel");
+} 
+else {console.log("It is a consonent");}
